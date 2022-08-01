6a9a1a2db3acd07eb81e21cb2622ec88388b0b69
Nicole Vu <nicolevu2312@gmail.com>
2022-08-01 15:27:27 -0500

    Fixed bug where the snake is splitted

--- Snake_Python/snake.py
+++ Snake_Python/snake.py
@@ -108,7 +108,7 @@ class snake(object):
         elif dx == 0 and dy == 1:
             self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
         elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1]+1)))
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
 
         self.body[-1].dirnx = dx
         self.body[-1].dirny = dy
