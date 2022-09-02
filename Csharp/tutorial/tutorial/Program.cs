cb5dbbd0fdb016eb4eb5adce3e57f55e7da51879
Nicole Vu <nicolevu2312@gmail.com>
2022-09-02 15:20:23 -0500

    Updating links

---
+++ Csharp/tutorial/tutorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(4/3);
+            Console.ReadLine();
+        }
+    }
+}
--- OnlineMultiPlayer_Python/server.py
+++ OnlineMultiPlayer_Python/server.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-server = "192.168.0.5"
+server = "192.168.0.10"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
